3c9d6c20ad731d73055fd7534f24b84e98e76e45
Adriel Teles <71078925+AdrielTeles97@users.noreply.github.com>
2026-04-09 14:31:00 -0300

    fix: glib

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,9 +1,16 @@
 // Substitua pelo seu ID do Google Analytics
 export const GA_TRACKING_ID = 'G-XXXXXXXXXX'
 
+// Adiciona a tipagem para window.gtag
+declare global {
+    interface Window {
+        gtag?: (...args: any[]) => void
+    }
+}
+
 // Função para enviar eventos
 export const pageview = (url: string) => {
-    if (typeof window !== 'undefined' && window.gtag) {
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
         window.gtag('config', GA_TRACKING_ID, {
             page_path: url
         })
